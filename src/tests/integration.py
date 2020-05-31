fd4133794a42ac89ed33619306a832b5a87c9d9a
optheca <previero@mit.edu>
2020-05-31 18:52:55 +0200

    integration tests

---
+++ src/tests/integration.py
@@ -0,0 +1,26 @@
+from src.tests.data_generation.delphi import RandomDELPHIData
+
+data_generator = RandomDELPHIData(
+    n_regions=50,
+    n_risk_classes=2,
+    n_timesteps=100,
+    days_per_timestep=1,
+    vaccine_budget_pct=0.01
+)
+model = data_generator.generate_model()
+solution, trajectories = model.solve(
+    exploration_tol=0.02,
+    termination_tol=1,
+    n_restarts=10,
+    max_iterations=5,
+    output_flag=False,
+    log=True
+)
+
+vaccines = solution.vaccinated
+old_objective = solution.get_total_deaths()
+new_vaccine, objective, bound = model.solve_benchmark(vaccines)
+
+new_objective = model.simulate(new_vaccine)
+print(old_objective, new_objective)
+
